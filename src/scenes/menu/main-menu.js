eae50d93a03da76fbeca67d3c968fe66ec7df118
Michael Burnley <about@michaelburnley.com>
2022-01-03 20:54:53 -0800

    added generic button

--- src/scenes/menu/main-menu.js
+++ src/scenes/menu/main-menu.js
@@ -1,4 +1,5 @@
 import { Scene } from "phaser";
+import { TextButton } from "../../game/ui/button";
 
 export class MainMenu extends Scene {
     constructor() {
@@ -11,6 +12,12 @@ export class MainMenu extends Scene {
 
     create() {
         console.log('Main Menu scene was created');
+
+        this.startButton = new TextButton(this, 100, 100, 'Start Game', { fill: '#0f0' });
+        this.add.existing(this.startButton);
+        this.startButton.on('pointerup', () => {
+            console.log(`clicked start`)
+        });
     }
 
     update(delta) {
